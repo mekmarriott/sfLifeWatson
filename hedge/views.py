a34ce6d043a7fb256b92d3a6f34e4dde872b96c8
Emma Marriott <mekmarriott@gmail.com>
2015-02-09 23:06:59 -0800

    hope this works with decoding fix?

--- hedge/views.py
+++ hedge/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import redirect
 # from django_ajax.decorators import ajax
 from requests.auth import HTTPBasicAuth
 from django.views.decorators.csrf import csrf_exempt
-import json
 import requests
 import logging
+import yaml
+import json, ast
 from . import form
 
 logging.basicConfig()
@@ -18,14 +19,18 @@ def new(request):
 @csrf_exempt
 def watson(request):
     headers = {
-        'content-type':'application/json'
+        'content-type':'application/json; charset=utf-8'
     }
+    #dict(queryDict.iterlists())
+    #print request.body
     auth = HTTPBasicAuth('ec08b85d-cf22-46c5-b728-eec7f3f24c9a', '7zO3zKu73n3E')
     watson_url = 'https://gateway.watsonplatform.net/tradeoff-analytics-beta/api/v1/dilemmas'
     print request.body
     response = requests.post(watson_url, data=request.body, headers=headers, auth=auth)
     print response.content
-    results = json.loads(response.text)
+    results = yaml.loads(response.text)
+    print "SUCCESSFUL UNTIL HERE"
+    print results
     return results
 
 
